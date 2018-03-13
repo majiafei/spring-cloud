cbebc0b5917603c7e6b68069f35a7828b7d0630c
Caffee <157121458@q.com>
2018-03-13 08:37:34 +0800

    启动类

--- src/main/java/org/giveu/mjf/springcloud/Master.java
+++ src/main/java/org/giveu/mjf/springcloud/Master.java
@@ -1,9 +1,18 @@
 package org.giveu.mjf.springcloud;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class Master {
 	
+	private static Logger logger = LoggerFactory.getLogger(Master.class);
+	
 	public static void main(String[] args) {
-		
+		SpringApplication.run(Master.class, args);
+		logger.info("springboot run success");
 	}
 
 }
